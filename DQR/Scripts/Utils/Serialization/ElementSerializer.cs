df1bea5e94f5a5135223f1ce684cacdfa5ded5ab
Sam Potter <sampot97@gmail.com>
2020-12-24 19:54:15 +0000

    Copied serialization stream utils

---
+++ DQR/Scripts/Utils/Serialization/ElementSerializer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public static class ElementSerializer
+{
+	/// <summary>
+	/// Wrapper function which will safely serialize a block of elements using SerializationChunk
+	/// Assumes elements is a static collection to read data from or write data to
+	/// </summary>
+	public static void Serialize(SerializationStream stream, IEnumerable<ISerializableElement> elements)
+	{
+		AssertNonUniqueKeys(elements);
+
+		if (stream.Mode == SerializationMode.Read)
+		{
+			byte[] data = null;
+			stream.SerializeBlock(ref data);
+			MemoryStream dataStream = new MemoryStream(data);
+
+			using (var groupStream = new GroupSerializationStream(dataStream, stream.Mode))
+			{
+				foreach (var elem in elements)
+				{
+					if (groupStream.OpenSerializationStream(elem.SerializationKey, out SerializationStream elemStream))
+					{
+						int version = elem.SerializationVersion;
+						elemStream.Serialize(ref version);
+						elem.SerializeElement(elemStream, version);
+					}
+				}
+			}
+		}
+		else
+		{
+			MemoryStream dataStream = new MemoryStream();
+
+			using (var groupStream = new GroupSerializationStream(dataStream, stream.Mode))
+			{
+				foreach (var elem in elements)
+				{
+					if (elem.ShouldSerializationExport && groupStream.OpenSerializationStream(elem.SerializationKey, out SerializationStream elemStream))
+					{
+						int version = elem.SerializationVersion;
+						elemStream.Serialize(ref version);
+						elem.SerializeElement(elemStream, version);
+					}
+				}
+			}
+
+			byte[] data = dataStream.ToArray();
+			stream.SerializeBlock(ref data);
+		}
+	}
+
+	public static void AssertNonUniqueKeys(IEnumerable<ISerializableElement> elements)
+	{
+#if UNITY_ASSERTIONS
+		var groupedElems = elements.GroupBy((e) => e.SerializationKey);
+		foreach (var group in groupedElems.Where((g) => g.Count() != 1))
+		{
+			Debug.AssertFormat(false, "Found duplicate serialization keys in the same group for '{0}'", group.Key);
+		}
+#endif
+	}
+}
---
+++ DQR/Scripts/Utils/Serialization/ISerializableElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// Attach this to anything which the element serializer can serialized
+/// </summary>
+public interface ISerializableElement
+{
+	/// <summary>
+	/// This will be checked during write serialization to avoid writing data if not required
+	/// </summary>
+	bool ShouldSerializationExport { get; }
+
+	/// <summary>
+	/// The internal key this component will be serialized under
+	/// </summary>
+	string SerializationKey { get; }
+
+	/// <summary>
+	/// The version number used to keep compatiblity between format changes
+	/// </summary>
+	int SerializationVersion { get; }
+
+	/// <summary>
+	/// Perform serialization for this element
+	/// </summary>
+	/// <param name="stream">The stream to read/write to/from</param>
+	/// <param name="version">If reading will be the version number from input stream, if writing will be the save as SerializationVersion</param>
+	void SerializeElement(SerializationStream stream, int version);
+}
